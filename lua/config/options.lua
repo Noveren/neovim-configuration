d513e7048f22ba03fbd59e0ba41cf97ece64fd5e
Noveren <1079148276@qq.com>
2025-02-23 21:30:38 +0800

    set basic configuration for neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,17 @@
+--
+-- use `:help <option>` to figure out the meaning of `<option>`
+--
 
+vim.opt.mouse = "nvi"                 -- Enable mouse support
+
+-- Tab
+vim.opt.expandtab = true              -- Use <space>s instead of <tab> 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+    
 -- UI Config
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.showmode = true
