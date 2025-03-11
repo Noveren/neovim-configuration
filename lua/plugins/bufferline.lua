28866fe6d2c9ec16c15e79413ad4087fd7a1f951
Noveren <1079148276@qq.com>
2025-03-11 11:24:23 +0800

    Replace 'config' with 'opts' in plugin-settings.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,22 +2,20 @@ return {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        require("bufferline").setup({
-            options = {
-                diagnostics = "nvim_lsp",
-                offsets = {{
-                    filetype = "NvimTree",
-                    text = "File Explorer",
-                    highlight = "Directory",
-                    text_align = "left"
-                }},
-                separator_style = "slant",
-                numbers = function(opts)
-                    return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
-                end,
-            }
-        })
-    end
+    opts = {
+        options = {
+            diagnostics = "nvim_lsp",
+            offsets = {{
+                filetype = "NvimTree",
+                text = "File Explorer",
+                highlight = "Directory",
+                text_align = "left"
+            }},
+            separator_style = "slant",
+            numbers = function(opts)
+                return string.format('%s', opts.raise(opts.id))
+            end,
+        }
+    },
 } 
 
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,24 +1,23 @@
 return {
     "nvim-tree/nvim-tree.lua",
+    enabled = true,
     version = "*",
     lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-        require("nvim-tree").setup({
-            sort = {
-                sorter = "case_sensitive",
-            },
-            view = {
-                width = 30,
-            },
-            renderer = {
-                group_empty = true,
-            },
-            filters = {
-                dotfiles = true,
-            },
-        })
-    end,
+    opts = {
+        sort = {
+            sorter = "case_sensitive",
+        },
+        view = {
+            width = 30,
+        },
+        renderer = {
+            group_empty = true,
+        },
+        filters = {
+            dotfiles = true,
+        },
+    },
 }
